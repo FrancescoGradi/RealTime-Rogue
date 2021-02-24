763792fc73e1d2524088768cadc253437a6bcb61
FrancescoGradi <gradifrancesco@yahoo.it>
2021-02-24 12:59:12 +0100

    Piccole modifiche

---
+++ Unity-Game/Assets/Scripts/ML/RealTimeAcademy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RealTimeAcademy : Academy
+{
+  public override void AcademyReset() {
+
+      FindObjectOfType<GameManager>().ResetTrainingScene();
+  }
+}
