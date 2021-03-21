06ad39e8de7a6eb5338d9f79d332725561b721b7
FrancescoGradi <gradifrancesco@yahoo.it>
2021-03-21 21:53:16 +0100

    Esperimenti con azioni discrete

---
+++ Unity-Game/Assets/Scripts/UI/Stats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Stats : MonoBehaviour
+{
+    public Player player;
+    public GameObject stats;
+    public TMP_Text ATK;
+    public TMP_Text MANA;
+    public TMP_Text DEF;
+    public TMP_Text SPEED;
+
+    void Update() {
+
+        if (Input.GetButton("Select")) {
+            stats.gameObject.SetActive(true);
+            
+            ATK.text = player.ATK.ToString();
+            MANA.text = player.MANA.ToString();
+            DEF.text = player.DEF.ToString();
+            SPEED.text = player.speed.ToString();
+        } else {
+            stats.gameObject.SetActive(false);
+        }
+    }
+}
