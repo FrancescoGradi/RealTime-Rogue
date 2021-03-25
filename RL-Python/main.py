52b064e721f34eb19ed8061842103ff990fdeb1d
FrancescoGradi <gradifrancesco@yahoo.it>
2021-03-25 19:45:18 +0100

    Movimento migliore, nuovo codice per training paralleli

--- RL-Python/main.py
+++ RL-Python/main.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 
+import os
+import argparse
+import json
+
 from tensorforce import Environment
 from unity_env_wrapper import UnityEnvWrapper, UnityDiscreteEnvWrapper
 
@@ -9,48 +13,51 @@ from train import train, evaluate
 
 if __name__ == '__main__':
 
-    curriculum = {
-        'thresholds': [4000, 6000, 8000, 10000, 12000],
-        'parameters': {
-            'range': [5, 6, 7, 8, 9, 10],
-            'agent_fixed': [0, 0, 0, 0, 0, 0],
-            'target_fixed': [0, 0, 0, 0, 0, 0],
-            'agent_update_rate': [25, 25, 25, 25],
-            'speed': [2, 2, 2, 2, 2, 2],
-            'update_movement': [100, 100, 100, 100, 100, 100]
-        }
-    }
-
-    curriculum = {
-        'thresholds': [5000, 10000, 15000, 20000],
-        'parameters': {
-            'range': [7, 6, 7, 8, 9],
-            'agent_fixed': [0, 0, 0, 0, 0],
-            'target_fixed': [0, 0, 0, 0, 0],
-            'agent_update_rate': [20, 20, 20, 20, 20],
-            'speed': [2, 2, 2, 2, 2],
-            'update_movement': [100, 100, 100, 100, 100]
-        }
-    }
-
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     if len(physical_devices) > 0:
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-    directory = "Model_Checkpoints/NewContinuous"
-    model_name = "RAPID20_2_CURRICULUM5-7_TS25_UR20"
-    total_directory = directory + "/" + model_name
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help="Path to json config file", required=True)
 
-    num_episodes = 15000
-    max_episode_timesteps = 25
-    # game_name = 'Compilati/24_03_cont'
-    game_name = None
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--train', action='store_true', help="If training")
+    group.add_argument('--eval', action='store_true', help="If evaluation")
+
+    args = parser.parse_args()
+
+    try:
+        with open(args.config) as c:
+            config = json.load(c)
+    except Exception as ex:
+        print(ex)
+
+    directory = config['directory']
+    model_name = config['model_name']
+    total_directory = directory + '/' + model_name
+
+    num_episodes = config['num_episodes']
+    max_episode_timesteps = config['max_episode_timesteps']
+    game_name = config['game_name']
+
+    worker_id = config['worker_id']
+
+    curriculum = config['curriculum']
 
     with tf.device('/device:GPU:0'):
-        env = UnityEnvWrapper(game_name=game_name)
-        env = Environment.create(environment=env, max_episode_timesteps=max_episode_timesteps)
+        if args.train:
+            env = UnityEnvWrapper(game_name=game_name, worker_id=worker_id)
+            env = Environment.create(environment=env, max_episode_timesteps=max_episode_timesteps)
+            train(env=env, directory=total_directory, num_episodes=num_episodes, curriculum=curriculum)
+        elif args.eval:
+            env = UnityEnvWrapper(game_name=None, worker_id=0)
+            env = Environment.create(environment=env, max_episode_timesteps=max_episode_timesteps)
+            evaluate(env=env, directory=total_directory, num_episodes=num_episodes, curriculum=curriculum)
+        else:
+            raise Exception('Argument not parsed correctly')
 
-        # train(env=env, directory=total_directory, num_episodes=num_episodes, curriculum=curriculum)
-        evaluate(env=env, directory=total_directory, num_episodes=num_episodes, curriculum=curriculum)
+    os.makedirs(os.path.dirname(total_directory + '/History/config.json'), exist_ok=True)
+    with open(total_directory + '/History/config.json', 'w') as c:
+        json.dump(config, c)
 
     visualize_history(directory=total_directory, num_mean=100, save_fig=True)
