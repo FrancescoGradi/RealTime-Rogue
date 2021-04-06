cce6d2022753f63a9ba3623056529194118fa88d
FrancescoGradi <gradifrancesco@yahoo.it>
2021-04-06 12:49:27 +0200

    Aggiunte animazioni cambio di scena Unity

---
+++ Unity-Game/Assets/Scripts/UI/RoomTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RoomTransition : MonoBehaviour {
+
+    public TMP_Text roomText;
+    public Animator transition;
+
+    public void Transition(string text) {
+        this.roomText.text = text;
+        transition.SetTrigger("Start");
+    }
+
+
+}
