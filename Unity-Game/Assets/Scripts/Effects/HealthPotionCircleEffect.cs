1032e918b284f4b7fac6b63bdfc99fb7e207f40f
FrancescoGradi <gradifrancesco@yahoo.it>
2021-04-13 18:42:37 +0200

    Azione pozioni e leggere modifiche combat system

---
+++ Unity-Game/Assets/Scripts/Effects/HealthPotionCircleEffect.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotionCircleEffect : MonoBehaviour {
+    void Start() {
+        Object.Destroy(this.gameObject, 1.5f);
+    }
+}
