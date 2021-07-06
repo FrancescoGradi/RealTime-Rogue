29d4c809dad39391b1aee74f16836d4de70faf50
FrancescoGradi <gradifrancesco@yahoo.it>
2021-07-06 12:46:21 +0200

    Modifiche bilanciamento gioco e bug fixing

---
+++ Unity-Game/Assets/Scripts/Menu/WelcomeMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WelcomeMenu : MonoBehaviour {
+
+    public static bool gamePaused = false;
+    public GameObject firstButton;
+
+    private void Start() {
+        this.gameObject.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstButton);
+
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Pause")) {
+            if (gamePaused) {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume() {
+        StartCoroutine(ResumeWaiter(0.2f));
+    }
+
+    private IEnumerator ResumeWaiter(float seconds) {
+        yield return new WaitForSecondsRealtime(seconds);
+
+        this.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+}
